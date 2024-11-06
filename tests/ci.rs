6623e56baca76d22762378e0d2ec3bba97348b31
Cade Mirchandani <cmirchan@ucsc.edu>
2024-11-06 09:12:32 -0800

    fix tests

--- tests/ci.rs
+++ tests/ci.rs
@@ -92,19 +92,6 @@ fn d4_files_are_equal<P: AsRef<Path> + Clone>(file1: P, file2: P, chrom: &str, b
 
 
 
-
-// Function to skip the first line in a binary file
-fn skip_first_line<R: Read>(reader: &mut BufReader<R>) -> io::Result<()> {
-    let mut byte = [0; 1];
-    loop {
-        if reader.read(&mut byte)? == 0 || byte[0] == b'\n' {
-            break;
-        }
-    }
-    Ok(())
-}
-
-
 #[test]
 fn test_no_pops_output_bed() -> Result<(), Box<dyn std::error::Error>> {
     //bamsim generate --num-bams 5 --num-chrs 1 --chr-length 1000 --min-depth 3 --max-depth 15 --min-mean-depth 6 --proportion 0.5 test_no_pops/
@@ -116,6 +103,129 @@ fn test_no_pops_output_bed() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("clam")?;
 
     let input_file = PathBuf::from_str("tests/data/test_no_pops/merged.d4")?;
+    let output_prefix = PathBuf::from_str("tests/data/test_no_pops/output_bed")?;
+    let output_file = output_prefix.with_extension("bed");
+    let expected_output_file = PathBuf::from_str("tests/data/test_no_pops/truth.bed")?;
+
+    cmd.arg("loci")
+        .arg(&input_file) // infile as a positional argument
+        .arg(&output_prefix) // unique outfile as a positional argument
+        .arg("-m")
+        .arg("3")
+        .arg("-M")
+        .arg("15")
+        .arg("-d")
+        .arg("0.5")
+        .arg("-u")
+        .arg("6")
+        .arg("--no-counts");
+
+    let output = cmd.output()?;
+    debug!("Ran command");
+    debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    let success = files_are_equal(expected_output_file.clone(), output_file.clone())?;
+    assert_eq!(true, success);
+    fs::remove_file(&output_file)?;
+
+    Ok(())
+}
+
+#[test]
+fn test_pops() -> Result<(), Box<dyn std::error::Error>> {
+    //./target/release/bamsim generate --num-bams 5 --num-chrs 1 --chr-length 1000 --min-depth 3 --max-depth 15 --min-mean-depth 6 --proportion 0.5 test_pops/pop1/bams/
+    let _ = env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .filter_level(log::LevelFilter::Trace)
+        .is_test(true)
+        .try_init();
+    let mut cmd = Command::cargo_bin("clam")?;
+
+    let input_file = PathBuf::from_str("tests/data/test_pops/merged.d4")?;
+    let output_prefix = PathBuf::from_str("tests/data/test_pops/output_d4")?;
+    let output_file = output_prefix.with_extension("d4");
+    let expected_output_file = PathBuf::from_str("tests/data/test_pops/counts.d4")?;
+    let pop_file = PathBuf::from_str("tests/data/test_pops/populations.tsv")?;
+    let chrom = "sq0";
+    let begin = 0;
+    let end = 1000;
+
+    cmd.arg("loci")
+        .arg(&input_file) // infile as a positional argument
+        .arg(&output_prefix) // unique outfile as a positional argument
+        .arg("-m")
+        .arg("3")
+        .arg("-M")
+        .arg("15")
+        .arg("-p")
+        .arg(&pop_file);
+
+    let output = cmd.output()?;
+    debug!("Ran command");
+    debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    let success = d4_files_are_equal(expected_output_file.clone(), output_file.clone(), &chrom, begin, end)?;
+    assert_eq!(true, success);
+    fs::remove_file(&output_file)?;
+
+    Ok(())
+}
+
+#[test]
+fn bgzf_test_no_pops_output_d4() -> Result<(), Box<dyn std::error::Error>> {
+    //bamsim generate --num-bams 5 --num-chrs 1 --chr-length 1000 --min-depth 3 --max-depth 15 --min-mean-depth 6 --proportion 0.5 test_no_pops/
+    let _ = env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .filter_level(log::LevelFilter::Trace)
+        .is_test(true)
+        .try_init();
+    let mut cmd = Command::cargo_bin("clam")?;
+
+    let input_file = PathBuf::from_str("tests/data/test_no_pops/merged.d4.gz")?;
+    let output_prefix = PathBuf::from_str("tests/data/test_no_pops/output_d4")?;
+    let output_file = output_prefix.with_extension("d4");
+    let expected_output_file = PathBuf::from_str("tests/data/test_no_pops/truth_counts.d4")?;
+    let chrom = "sq0";
+    let begin = 0;
+    let end = 1000;
+
+    cmd.arg("loci")
+        .arg(&input_file) // infile as a positional argument
+        .arg(&output_prefix) // unique outfile as a positional argument
+        .arg("-m")
+        .arg("3")
+        .arg("-M")
+        .arg("15")
+        .arg("-d")
+        .arg("0.5")
+        .arg("-u")
+        .arg("6");
+
+    let output = cmd.output()?;
+    debug!("Ran command");
+    debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    let success = d4_files_are_equal(expected_output_file.clone(), output_file.clone(), &chrom, begin, end)?;
+    assert_eq!(true, success);
+    fs::remove_file(&output_file)?;
+
+    Ok(())
+}
+
+#[test]
+fn bgzf_test_no_pops_output_bed() -> Result<(), Box<dyn std::error::Error>> {
+    //bamsim generate --num-bams 5 --num-chrs 1 --chr-length 1000 --min-depth 3 --max-depth 15 --min-mean-depth 6 --proportion 0.5 test_no_pops/
+    let _ = env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .filter_level(log::LevelFilter::Trace)
+        .is_test(true)
+        .try_init();
+    let mut cmd = Command::cargo_bin("clam")?;
+
+    let input_file = PathBuf::from_str("tests/data/test_no_pops/merged.d4.gz")?;
     let output_prefix = PathBuf::from_str("tests/data/test_no_pops/output_bed")?;
     let output_file = output_prefix.with_extension("bed");
     let expected_output_file = PathBuf::from_str("tests/data/test_no_pops/truth.bed")?;
@@ -187,151 +297,43 @@ fn test_no_pops_output_d4() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// #[test]
-// fn test_gzipped_d4() -> Result<(), Box<dyn std::error::Error>> {
-//     let _ = env_logger::builder()
-//         .target(env_logger::Target::Stdout)
-//         .filter_level(log::LevelFilter::Trace)
-//         .is_test(true)
-//         .try_init();
-//     let mut cmd = Command::cargo_bin("clam")?;
+#[test]
+fn bgzf_test_pops() -> Result<(), Box<dyn std::error::Error>> {
+    //./target/release/bamsim generate --num-bams 5 --num-chrs 1 --chr-length 1000 --min-depth 3 --max-depth 15 --min-mean-depth 6 --proportion 0.5 test_pops/pop1/bams/
+    let _ = env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .filter_level(log::LevelFilter::Trace)
+        .is_test(true)
+        .try_init();
+    let mut cmd = Command::cargo_bin("clam")?;
 
-//     // Generate a unique file name for the output
-//     let output_file = PathBuf::from_str("tests/data/test_gzipped.bed")?;
+    let input_file = PathBuf::from_str("tests/data/test_pops/merged.d4.gz")?;
+    let output_prefix = PathBuf::from_str("tests/data/test_pops/output_d4")?;
+    let output_file = output_prefix.with_extension("d4");
+    let expected_output_file = PathBuf::from_str("tests/data/test_pops/counts.d4")?;
+    let pop_file = PathBuf::from_str("tests/data/test_pops/populations.tsv")?;
+    let chrom = "sq0";
+    let begin = 0;
+    let end = 1000;
 
-//     // Specify the `loci` subcommand and add positional arguments for infile and outfile
-//     cmd.arg("loci")
-//         .arg("tests/data/merged.d4.gz") // infile as a positional argument
-//         .arg(&output_file) // unique outfile as a positional argument
-//         .arg("-m")
-//         .arg("3")
-//         .arg("-M")
-//         .arg("20")
-//         .arg("-d")
-//         .arg("0.66")
-//         .arg("-u")
-//         .arg("5");
+    cmd.arg("loci")
+        .arg(&input_file) // infile as a positional argument
+        .arg(&output_prefix) // unique outfile as a positional argument
+        .arg("-m")
+        .arg("3")
+        .arg("-M")
+        .arg("15")
+        .arg("-p")
+        .arg(&pop_file);
 
-//     // Execute the command and capture the output
-//     let output = cmd.output()?;
-//     debug!("Ran command");
-//     debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-//     // Read the generated output for comparison
-//     let mut generated_output = String::new();
-//     fs::File::open(&output_file)
-//         .context(format!("Couldn't open file: {}", output_file.display()))?
-//         .read_to_string(&mut generated_output)?;
+    let output = cmd.output()?;
+    debug!("Ran command");
+    debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 
-//     // Read the expected output from the truth file
-//     let expected_output_path = Path::new("tests/data/truth.bed");
-//     let mut expected_output = String::new();
-//     fs::File::open(expected_output_path)?.read_to_string(&mut expected_output)?;
+    let success = d4_files_are_equal(expected_output_file.clone(), output_file.clone(), &chrom, begin, end)?;
+    assert_eq!(true, success);
+    fs::remove_file(&output_file)?;
 
-//     // Compare the generated output to the expected output
-//     assert_eq!(generated_output, expected_output);
-
-//     // Clean up by removing the generated output file
-//     fs::remove_file(&output_file)?;
-
-//     Ok(())
-// }
-
-// #[test]
-// fn test_gzipped_d4_pops() -> Result<(), Box<dyn std::error::Error>> {
-//     let _ = env_logger::builder()
-//         .target(env_logger::Target::Stdout)
-//         .filter_level(log::LevelFilter::Trace)
-//         .is_test(true)
-//         .try_init();
-//     let mut cmd = Command::cargo_bin("clam")?;
-
-//     // Generate a unique file name for the output
-//     let output_file = PathBuf::from_str("tests/data/test_gzipped_pops.d4")?;
-//     let pops_file = PathBuf::from_str("tests/data/populations.tsv")?;
-
-//     // Specify the `loci` subcommand and add positional arguments for infile and outfile
-//     cmd.arg("loci")
-//         .arg("tests/data/merged.d4.gz") // infile as a positional argument
-//         .arg(&output_file) // unique outfile as a positional argument
-//         .arg("-m")
-//         .arg("3")
-//         .arg("-M")
-//         .arg("20")
-//         .arg("-p")
-//         .arg(&pops_file);
-
-//     // Execute the command and capture the output
-//     let output = cmd.output()?;
-
-    
-//     assert!(
-//         output.status.success(),
-//         "Command failed with stderr: {}",
-//         String::from_utf8_lossy(&output.stderr)
-//     );
-
-//     debug!("Ran command");
-//     debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-
-    
-//     let metadata = fs::metadata(&output_file)
-//         .context(format!("Couldn't get metadata for file: {}", output_file.display()))?;
-//     assert!(metadata.len() > 0, "Output file should not be empty.");
-
-    
-//     fs::remove_file(&output_file)?;
-
-//     Ok(())
-// }
-
-// #[test]
-// fn test_d4() -> Result<(), Box<dyn std::error::Error>> {
-//     let _ = env_logger::builder()
-//         .target(env_logger::Target::Stdout)
-//         .filter_level(log::LevelFilter::Trace)
-//         .is_test(true)
-//         .try_init();
-//     let mut cmd = Command::cargo_bin("clam")?;
-
-//     // Generate a unique file name for the output
-//     let output_file = PathBuf::from_str("tests/data/test.bed")?;
-
-//     // Specify the `loci` subcommand and add positional arguments for infile and outfile
-//     cmd.arg("loci")
-//         .arg("tests/data/merged.d4") // infile as a positional argument
-//         .arg(&output_file) // unique outfile as a positional argument
-//         .arg("-m")
-//         .arg("3")
-//         .arg("-M")
-//         .arg("20")
-//         .arg("-d")
-//         .arg("0.66")
-//         .arg("-u")
-//         .arg("5");
-
-//     // Execute the command and capture the output
-//     let output = cmd.output()?;
-//     debug!("Ran command");
-//     debug!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     debug!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-//     // Read the generated output for comparison
-//     let mut generated_output = String::new();
-//     fs::File::open(&output_file)
-//         .context(format!("Couldn't open file: {}", output_file.display()))?
-//         .read_to_string(&mut generated_output)?;
-
-//     // Read the expected output from the truth file
-//     let expected_output_path = Path::new("tests/data/truth.bed");
-//     let mut expected_output = String::new();
-//     fs::File::open(expected_output_path)?.read_to_string(&mut expected_output)?;
-
-//     // Compare the generated output to the expected output
-//     assert_eq!(generated_output, expected_output);
-
-//     // Clean up by removing the generated output file
-//     fs::remove_file(&output_file)?;
-
-//     Ok(())
-// }
+    Ok(())
+}
